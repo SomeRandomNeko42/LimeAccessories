ce1d25dc433daf7cfa8ff887a3fa8bb59d1806b1
SomeRandomNeko <77223041+SomeRandomNeko42@users.noreply.github.com>
2025-07-14 21:28:37 -0700

    If its more common for the skeleton merchant, the travelling one shouldnt be excluded

--- GlobalNPCs/LimeShopModifications.cs
+++ GlobalNPCs/LimeShopModifications.cs
@@ -16,7 +16,7 @@ namespace LimeAccessories.GlobalNPCs
 		public override void SetupTravelShop(int[] shop, ref int nextSlot)
 		{
 			int omamori = ModContent.ItemType<AncientOmamori>();
-			if (!shop.Contains(omamori) && Main._rand.NextBool(1, 10))
+			if (!shop.Contains(omamori) && Main._rand.NextBool(1, 6))
 			{
 				shop[nextSlot] = omamori;
 				nextSlot++;
